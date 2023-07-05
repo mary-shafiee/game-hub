f8653481a480901e3c4087223e7f84914b077bf2
mary-shafiee <marysheei16@gmail.com>
2023-07-05 13:06:27 +0330

    building dark mode switch

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,11 +1,12 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/react.svg";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent={"space-between"} paddingX={"10px"}>
       <Image src={logo} boxSize="60px" />
-      <Text>here is navbar</Text>
+      <ColorModeSwitch/>
     </HStack>
   );
 };
