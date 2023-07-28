edd0666619445a15e9e5f72db50fb2108042c8b4
mary-shafiee <marysheei16@gmail.com>
2023-07-28 21:43:17 +0330

    add no image placeholder

--- src/Services/image-url.ts
+++ src/Services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from "../assets/images/1bf0a2eb21537488818c8ff5de1dd845.jpg"
+
 const getCroppedImageUrl = ( url : string) => {
-    if(!url) return '';
+    if(!url) return noImage;
     const target = 'media/';
     const index = url.indexOf(target) + target.length ;
     return url.slice(0 , index) + 'crop/600/400/' + url.slice(index) ; 
