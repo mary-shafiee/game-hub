7ba963a71411995cc3ce7a0e4685a16828bb1b8c
mary-shafiee <marysheei16@gmail.com>
2023-07-29 14:39:10 +0330

    shipping static data

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,12 +1,11 @@
-import useData from "./useData";
-
+import genres from '../datas/genres'
 export interface Genre {
   id: number;
   name: string;
   image_background : string;
 }
 
-const useGenres = () => useData<Genre>("/genres");
+const useGenres = () => ({data : genres , isLoading : false , error : null});
 
 
 export default useGenres;
